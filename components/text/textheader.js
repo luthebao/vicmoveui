37500bccf84219a3579b19bb038136065c5fdfaf
luthebao <luthebao97@gmail.com>
2022-04-11 17:32:11 +0700

    update Text

--- components/text/textheader.js
+++ components/text/textheader.js
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 
-const TextHeader = ({ children, size, className, onClick }) => {
-    let classes = 'text-black';
+const TextHeader = ({ children, size, textColor, className, onClick }) => {
+    let classes = textColor ? textColor : 'text-black'
     switch (size) {
         case 'lg':
-            classes += ' text-5xl text-center py-8 ';
+            classes += ' text-5xl text-center py-6 leading-14 ';
             break;
         case 'md':
-            classes += ' text-4xl text-center py-4 ';
+            classes += ' text-4xl text-center py-4 leading-12 ';
             break;
         case 'md-left':
-            classes += ' text-4xl py-4 ';
+            classes += ' text-4xl py-4 leading-12 ';
             break;
         case 'xs':
-            classes += ' text-2xl py-2';
+            classes += ' text-xl py-2 ';
             break;
         default:
             classes += ' text-3xl py-2 ';
