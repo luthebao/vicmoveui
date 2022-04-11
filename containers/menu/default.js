1fa6dbc9ffc909ad9b7ad15c1f400715b5bf8b91
luthebao <luthebao97@gmail.com>
2022-04-11 22:39:54 +0700

    update icons menu

--- containers/menu/default.js
+++ containers/menu/default.js
@@ -1,24 +1,30 @@
 import PropTypes from 'prop-types';
 import ButtonLink from '../../components/button/btnlink';
 import { max_width } from '../../utils/config';
+import { BiUser } from "react-icons/bi";
+import { SiAzurefunctions } from "react-icons/si";
+import { FiShoppingCart } from 'react-icons/fi';
+import { GiConverseShoe } from 'react-icons/gi';
+
 
 const Menu = ({ className, active }) => {
   return (
-    <div className={max_width + ' rounded-t-4xl px-8 pt-4 pb-6  shadow-2xl bg-vicm-green-600 w-full bottom-0 flex justify-between fixed ' + (className || '')}>
-      <ButtonLink href={'/'} buttonStyle='flat' className={`btn-menu ${active === "profile" ? "btn-circle-active" : ""}`} shapes="circle">
-        <img src={"/images/icons/profile.png"} />
+    <div className={max_width + ' text-white rounded-t-4xl pt-4 pb-6  shadow-2xl bg-vicm-green-600 w-full bottom-0 flex fixed ' + (className || '')}>
+
+      <ButtonLink href={'/'} buttonStyle='flat' className={`m-auto btn-menu ${active === "profile" ? "btn-circle-active" : ""}`} shapes="circle">
+        <BiUser />
       </ButtonLink>
-      <ButtonLink href={'/user/bag'} buttonStyle='flat' className={`btn-menu ${active === "bag" ? "btn-circle-active" : ""}`} shapes="circle">
-        <img src={"/images/icons/shoes.png"} />
+      <ButtonLink href={'/user/bag'} buttonStyle='flat' className={`m-auto btn-menu ${active === "bag" ? "btn-circle-active" : ""}`} shapes="circle">
+        <GiConverseShoe />
       </ButtonLink>
-      <ButtonLink href={'/runnative'} buttonStyle='flat' className='btn-menu btn-hightlight' shapes="circle">
+      <ButtonLink href={'/runnative'} buttonStyle='flat' className='mx-6 btn-menu btn-hightlight' shapes="circle">
         <img src={"/images/icons/start.png"} />
       </ButtonLink>
-      <ButtonLink href={'/fusion'} buttonStyle='flat' className={`btn-menu ${active === "coming" ? "btn-circle-active" : ""}`} shapes="circle">
-        <img src={"/images/icons/coming.png"} />
+      <ButtonLink href={'/user/market'} buttonStyle='flat' className={`m-auto btn-menu ${active === "market" ? "btn-circle-active" : ""}`} shapes="circle">
+        <FiShoppingCart />
       </ButtonLink>
-      <ButtonLink href={'/user/market'} buttonStyle='flat' className={`btn-menu ${active === "market" ? "btn-circle-active" : ""}`} shapes="circle">
-        <img src={"/images/icons/store.png"} />
+      <ButtonLink href={'/fusion'} buttonStyle='flat' className={`m-auto btn-menu ${active === "coming" ? "btn-circle-active" : ""}`} shapes="circle">
+        <SiAzurefunctions />
       </ButtonLink>
     </div>
   );
