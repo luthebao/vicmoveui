bed1ddd0d7e23739228c8f78461daa167c5ef0e5
luthebao <luthebao97@gmail.com>
2022-04-12 16:14:22 +0700

    update

---
+++ pages/runnative.js
@@ -0,0 +1,17 @@
+import LayoutMenu from "../layouts/layoutmenu"
+
+
+
+const Runnative = () => {
+
+    return (
+        <LayoutMenu active={"coming"}>
+            <div className='flex justify-center m-auto flex flex-col'>
+                <p>Comming soon ...</p>
+            </div>
+        </LayoutMenu>
+    )
+}
+
+
+export default Runnative
